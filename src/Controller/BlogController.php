aed4092d86720f5739dedc769dbe4ee92e8d4a70
TomyS <tomy@s.a>
2023-06-12 15:55:26 +0200

    Ajout pagination liste des articles

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -4,60 +4,61 @@ namespace App\Controller;
 
 use App\Entity\Article;
 use App\Form\NewPublicationFormType;
+use Doctrine\ORM\Tools\Pagination\Paginator;
 use Doctrine\Persistence\ManagerRegistry;
+use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
 
-/*
+
+/**
  * Préfixe de la route et du nom de toutes les pages de la partie blog du site
- * */
-#[Route('/blog', name: 'blog_')]
+ */
+#[ROUTE('/blog', name: 'blog_')]
 class BlogController extends AbstractController
 {
 
     /*
      * Contrôleur de la page permettant de créer un nouvel article
-     * */
-    #[Route('/nouvelle-publication/', name: 'new_publication')]
+     */
+    #[Route('/nouvelle-publication', name: 'new_publication')]
     #[IsGranted('ROLE_ADMIN')]
-    public function newPublication(Request $request, ManagerRegistry $doctrine): Response
+    public function NewPublication(Request $request, ManagerRegistry $doctrine): Response
     {
-        // Création d'un nouvel article vide
+
+        //Création d'un nouvel article vide
         $newArticle = new Article();
 
-        // Création d'un formulaire de création d'article, lié à l'article vide
         $form = $this->createForm(NewPublicationFormType::class, $newArticle);
 
-        // Liaison des données POST au formulaire
+        //Liaison des données POST au formulaire
         $form->handleRequest($request);
 
-        // Si le formulaire a bien été envoyé et sans erreurs
         if ($form->isSubmitted() && $form->isValid()) {
 
-            // On termine d'hydrater l'article
             $newArticle
                 ->setPublicationDate(new \DateTime())
                 ->setAuthor($this->getUser());
-
-            // Sauvegarde en base de données grâce au manager des entités
             $em = $doctrine->getManager();
             $em->persist($newArticle);
             $em->flush();
 
-            // Message flash de succès
             $this->addFlash('success', 'Article publié avec succès !');
 
-            // Redirige sur la page qui montre le nouvel article
-            return $this->redirectToRoute('blog_publication_view', [
+
+            return $this->redirectToRoute('blog_publication_view',[
+
                 'slug' => $newArticle->getSlug(),
+
             ]);
         }
 
         return $this->render('blog/new_publication.html.twig', [
-            'new_publication_form' => $form->createView(),
+            'new_publication_form' => $form->createView()
         ]);
     }
 
@@ -65,31 +66,48 @@ class BlogController extends AbstractController
      * Contrôleur de la page qui liste tous les articles
      */
     #[Route('/publications/liste/', name: 'publication_list')]
-    public function publicationList(ManagerRegistry $doctrine): Response
-    {
-        // Récupération du repository des articles
-        $articleRepo = $doctrine->getRepository(Article::class);
+    public function publicationList(ManagerRegistry $doctrine, Request $request, PaginatorInterface $paginator): Response
 
-        // On demande au repository de nous donner tous les articles qui sont en BDD
-        $articles = $articleRepo->findAll();
+    {
+        $requestedPage = $request->query->getInt('pagge', 1);
+
+        if ($requestedPage < 1){
+            throw new NotFoundHttpException();
+        }
+
+        $em = $doctrine->getManager();
+
+        $query = $em->createQuery('SELECT a FROM App\Entity\Article a ORDER BY a.publicationDate DESC');
+
+        $articles = $paginator->paginate(
+            $query,
+            $requestedPage,
+            10
+        );
 
         return $this->render('blog/publication_list.html.twig', [
+
             'articles' => $articles,
+
         ]);
+
     }
 
-
     /**
-     * Contrôleur de la page permettant de voir unn article en détail
+     * Contrôleur de la page permettant de voir un article en détail
      */
+
     #[Route('/publication/{slug}/', name: 'publication_view')]
     public function publicationView(Article $article): Response
     {
 
-    return $this->render('blog/publication_view.html.twig', [
-        'article' => $article,
-    ]);
+        return $this->render('blog/publication_view.html.twig', [
+            'article' => $article,
+
+        ]);
+
+    }
+
 }
 
 
-}
